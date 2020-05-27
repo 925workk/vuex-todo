5eb6ce8f90752bc96803b8bf221c24ee614245bf
Alex Pham <925workk@gmail.com>
2020-05-27 14:57:36 -0600

    added updating system.

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,9 +1,23 @@
 <template>
   <div>
       <h3>Todos</h3>
+      <div class="legend">
+          <span>Double click to mark as complete</span>
+          <span>
+              <span class="incomplete-box"></span>  = Incomplete
+          </span>
+          <span>
+              <span class="complete-box"></span>  = Complete
+          </span>
+      </div>
       <div class="todos">
-          <div class="todo" v-for="todo in allTodos" :key='todo.id'>
-              {{ todo.title}}
+          <div 
+            @dblclick="onDblClick(todo)" 
+            class="todo" 
+            v-for="todo in allTodos" 
+            :key='todo.id' 
+            v-bind:class="{'is-complete':todo.completed}">
+            {{ todo.title}}
             <i @click=deleteTodo(todo.id) class="far fa-trash-alt"></i>
           </div>
       </div>
@@ -15,7 +29,15 @@ import { mapGetters, mapActions } from 'vuex';
     export default {
         name: "Todos",
         methods: {
-            ...mapActions(['fetchTodos', 'deleteTodo'])
+            ...mapActions(['fetchTodos', 'deleteTodo', 'updateTodo']),
+            onDblClick(todo){
+                const updTodo = {
+                    id:todo.id,
+                    title: todo.title,
+                    completed: !todo.completed
+                }
+                this.updateTodo(updTodo)
+            }
         },
         computed: mapGetters(['allTodos']),
         created (){
@@ -46,5 +68,31 @@ import { mapGetters, mapActions } from 'vuex';
         right: 10px;
         color: #fff;
         cursor: pointer;
+    }
+    .legend {
+        display: flex;
+        justify-content: space-around;
+        margin-bottom: 1rem;
+    }
+    .complete-box {
+        display: inline-block;
+        width: 10px;
+        height: 10px;
+        background: #35495e;
+    }
+    .incomplete-box {
+        display: inline-block;
+        width: 10px;
+        height: 10px;
+        background: #41b883;
+    }
+    .is-complete{
+        background: #35495e;
+        color: #fff;
+    }
+    @media (max-width: 500px){
+        .todos {
+            grid-template-columns: 1fr;
+        }
     }
 </style>
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -32,6 +32,11 @@ const actions = {
 
         const response = await axios.get(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}`)
         commit('setTodos', response.data);
+    },
+    async updateTodo({commit}, updTodo){
+        const response = await axios.put(`https://jsonplaceholder.typicode.com/todos/${updTodo}`, updTodo)
+
+        commit('updateTodo', response.data)
     }
 };
 
@@ -44,6 +49,12 @@ const mutations = {
     },
     removeTodo: (state, id)=>{
         state.todos = state.todos.filter(todo => todo.id !== id)
+    },
+    updateTodo: (state, updTodo) => {
+        const index = state.todos.findIndex(todo => todo.id === updTodo.id);
+        if (index !== -1){
+            state.todos.splice(index, 1, updTodo)
+        }
     }
 };
 
